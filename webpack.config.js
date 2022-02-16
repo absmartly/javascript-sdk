152ba85eaffce1a45f2e18ce2b07bc7501037c22
Márcio Martins <marcioapm@gmail.com>
2022-02-16 17:51:00 +0100

    WEBPACK_ANALYZE env var to enable bundle analyzer webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const TerserPlugin = require("terser-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
 const env = process.env.NODE_ENV || "development";
+const analyze = process.env.WEBPACK_ANALYZE || false;
 
 module.exports = function () {
 	const config = {
@@ -33,13 +36,17 @@ module.exports = function () {
 		config.output.filename = "[name].min.js";
 		config.performance = {
 			hints: "error",
-			maxAssetSize: 51200,
+			maxAssetSize: 69632,
 		};
 
 		config.optimization = {
 			minimize: true,
 			minimizer: [new TerserPlugin()],
 		};
+
+		if (analyze) {
+			config.plugins = [new BundleAnalyzerPlugin()];
+		}
 	} else {
 		config.output.filename = "[name].js";
 	}
