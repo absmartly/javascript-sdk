d835ea4bbd6ee9bf8a7b2ce7adce639c71fc7d30
Cal Courtney <cal@absmartly.com>
2022-10-12 16:20:27 +0100

    Added check for Promise.finally() and added a .then otherwise

--- src/client.js
+++ src/client.js
@@ -199,12 +199,16 @@ export default class Client {
 				  }, timeout)
 				: 0;
 
-		return tryWith(this._opts.retries, this._opts.timeout).finally(() => {
+		const onFinallyAfterTryWith = () => {
 			clearTimeout(timeoutId);
 			if (options.signal) {
 				options.signal.removeEventListener("abort", abort);
 			}
-		});
+		};
+
+		return Promise.prototype.finally
+			? tryWith(this._opts.retries, this._opts.timeout).finally(onFinallyAfterTryWith)
+			: tryWith(this._opts.retries, this._opts.timeout).then(onFinallyAfterTryWith, onFinallyAfterTryWith);
 	}
 
 	post(options) {
