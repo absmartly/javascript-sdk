4a78a031ae84f877c84e63e1ccaa4f364e851bb9
Cal Courtney <calthejuggler@gmail.com>
2023-01-17 14:16:28 +0000

    Converted index.js to ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import SDK from "./sdk";
 import { mergeConfig } from "./config";
 import { ContextDataProvider } from "./provider";
 import { ContextPublisher } from "./publisher";
-// eslint-disable-next-line no-shadow
 import { AbortController } from "./abort";
 
 export { mergeConfig, AbortController, Context, ContextDataProvider, ContextPublisher, SDK };
