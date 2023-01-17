8dd935b9c229d1154feba48995cdfed9a7b837e9
Cal Courtney <calthejuggler@gmail.com>
2023-01-17 13:15:09 +0000

    Converted murmur3_32.js to ts

--- src/murmur3_32.ts
+++ src/murmur3_32.ts
@@ -4,7 +4,7 @@ const C3 = 0xe6546b64;
 
 const imul32 = Math.imul;
 
-function fmix32(h) {
+function fmix32(h: number) {
 	h ^= h >>> 16;
 	h = imul32(h, 0x85ebca6b);
 	h ^= h >>> 13;
@@ -14,44 +14,44 @@ function fmix32(h) {
 	return h >>> 0;
 }
 
-function rotl32(a, b) {
+function rotl32(a: number, b: number) {
 	return (a << b) | (a >>> (32 - b));
 }
 
-function scramble32(block) {
+function scramble32(block: number) {
 	return imul32(rotl32(imul32(block, C1), 15), C2);
 }
 
-export function murmur3_32(key, hash) {
+export function murmur3_32(key: ArrayBufferLike, hash: number) {
 	hash = (hash || 0) >>> 0;
-	key = new DataView(key);
+	const dataView = new DataView(key);
 
 	let i;
-	const n = key.byteLength & ~3;
+	const n = dataView.byteLength & ~3;
 	for (i = 0; i < n; i += 4) {
-		const chunk = key.getUint32(i, true);
+		const chunk = dataView.getUint32(i, true);
 		hash ^= scramble32(chunk);
 		hash = rotl32(hash, 13);
 		hash = imul32(hash, 5) + C3;
 	}
 
 	let remaining = 0;
-	switch (key.byteLength & 3) {
+	switch (dataView.byteLength & 3) {
 		case 3:
-			remaining ^= key.getUint8(i + 2) << 16;
+			remaining ^= dataView.getUint8(i + 2) << 16;
 		// fallthrough
 		case 2:
-			remaining ^= key.getUint8(i + 1) << 8;
+			remaining ^= dataView.getUint8(i + 1) << 8;
 		// fallthrough
 		case 1:
-			remaining ^= key.getUint8(i);
+			remaining ^= dataView.getUint8(i);
 			hash ^= scramble32(remaining);
 		// fallthrough
 		default:
 			break;
 	}
 
-	hash ^= key.byteLength;
+	hash ^= dataView.byteLength;
 	hash = fmix32(hash);
 	return hash >>> 0;
 }
