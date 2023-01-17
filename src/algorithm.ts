b92ef7c7e2ab6048a3f3142f7487f3ac7858a75c
Cal Courtney <calthejuggler@gmail.com>
2023-01-17 13:16:41 +0000

    Converted algorithm.js to ts

--- src/algorithm.ts
+++ src/algorithm.ts
@@ -1,4 +1,4 @@
-export const insertUniqueSorted = (arr, value, isSorted) => {
+export const insertUniqueSorted = (arr: any[], value: any, isSorted: (a: any, b: any) => boolean) => {
 	let left = 0;
 	let right = arr.length - 1;
 
