f9358301ed7ec06265218b9a533cb0a1077ca492
Cal Courtney <cal@absmartly.com>
2023-10-19 12:26:47 +0100

    DASH-1249: ReactNative support

--- src/abort.ts
+++ src/abort.ts
@@ -1,9 +1,9 @@
-import { isBrowser, isWorker } from "./utils";
+import { isLongLivedApp, isWorker } from "./utils";
 import AbortControllerShim from "./abort-controller-shim";
 
 // eslint-disable-next-line no-shadow
 export const AbortController =
-	isBrowser() && window.AbortController
+	isLongLivedApp() && window.AbortController
 		? window.AbortController
 		: isWorker() && self.AbortController
 		? self.AbortController
