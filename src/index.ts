a765750797e7efd3c7096829ed5dcce2fb2f9a62
Jonas Alves <jonas@absmartly.com>
2026-03-29 00:25:38 +0000

    feat: add public API exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-export const placeholder = true;
+export { SDK } from "./sdk";
+export { Context } from "./context";
+export { ContextDataProvider } from "./provider";
+export { ContextPublisher } from "./publisher";
+export { mergeConfig } from "./config";
+
+export type {
+	ApplicationObject,
+	Attribute,
+	Assignment,
+	ClientOptions,
+	ClientRequestOptions,
+	ContextData,
+	ContextParams,
+	CustomFieldValue,
+	CustomFieldValueType,
+	EventLogger,
+	EventLoggerData,
+	EventName,
+	Experiment,
+	ExperimentData,
+	Exposure,
+	Goal,
+	JSONValue,
+	NormalizedClientOptions,
+	PublishParams,
+	Unit,
+	Units,
+} from "./types";
